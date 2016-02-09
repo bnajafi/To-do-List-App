853bac923c308f88d18031e47b615d3dc1db1794
behzad najafi <behzad najafi>
2016-02-09 16:00:19 +0100

    updating the app.js

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+//Add a New tasks
+var addTask=function(){
+	//When the button is pressed
+	//create a new list item with the text from te #new-task
+	//input (Checkbox)
+	//label
+	//input(Text)
+	//button.edit
+	//button.delete
+	//each input should be modififed and appended
+						}
+
+//edit an existing task
+var editTask=function(){
+	//when the edit button is pressed
+	// if the class of the parent is .editmode:
+		//switch from .editMode 
+		//label text become the input's value
+	//else
+		//switch to editMode 
+		//input Value becomes thhe label's text
+	//toggle .editMode	
+}
+
+var deleteTask= function(){
+
+//Delete an existing task
+	//When the delte button is pressed:
+	//Remove the parent list item from the unordered list
+
+}
+
+var taskCompleted=function(){
+	//Mark a task as complete
+	//when the checkbox is checked
+		//append the task list item to #completed-tasks 
+}
+
+var taskIncompleted=function(){
+//Mark a task as incomplete
+	//when the check box is unchecked:
+		//append to #incomplete-tasks
+}
+
